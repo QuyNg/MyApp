f79efd27a4a1c6a9f1ceaaf7a4d1fc1a2339536a
QuyNguyen <QuyNH@Quys-Mac-mini.local>
2016-04-03 00:10:52 +0700

    add files

--- supercool/ViewController.swift
+++ supercool/ViewController.swift
@@ -10,9 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolBoom: UIImageView!
+    @IBOutlet weak var coolBG: UIImageView!
+    @IBOutlet weak var showThings: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+//        coolBG.hidden = true
+//        coolBoom.hidden = true
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +26,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showBoom(sender: AnyObject) {
+        coolBG.hidden = false
+        coolBoom.hidden = false
+        showThings.hidden = true
+    }
 
 }
 
